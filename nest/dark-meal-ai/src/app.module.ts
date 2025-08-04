7b691bbcd437217e1c72f9b0e6731563b543118c
karakarakr <titan1234eti@gmail.com>
2025-08-04 15:10:07 +0300

    Init project backend folder structure

--- nest/dark-meal-ai/src/app.module.ts
+++ nest/dark-meal-ai/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AbcModule } from './abc/abc.module';
+import { TestModule } from './test/test.module';
+import { RecipesModule } from './recipes/recipes.module';
+import { UsersModule } from './users/users.module';
+import { AbcModule } from './abc/abc.module';
 
 @Module({
-  imports: [],
+  imports: [AbcModule, UsersModule, RecipesModule, TestModule],
   controllers: [AppController],
   providers: [AppService],
 })
