1fa4eebc8ad465aff9dce5a88aabf02cd457f6aa
karakarakr <titan1234eti@gmail.com>
2025-08-04 13:42:08 +0300

    Init commit

---
+++ react/dark-meal-ai/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [firstResponse, setFirstResponse] = useState(null);
+  useEffect(() => {
+    axios.get('http://localhost:3000/')
+      .then(response => setFirstResponse(response.data))
+      .catch(error => console.error('Error fetching tasks:', error));
+  }, []);
+
+  return (
+    <>
+      <h1>{firstResponse}</h1>
+    </>
+  )
+}
+
+export default App
